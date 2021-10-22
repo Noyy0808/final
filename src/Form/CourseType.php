cb90cdfe52a490d9a4d734571dd218daefe50f9d
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2021-10-22 17:42:48 +0700

    update  Course
    update  Course

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -2,8 +2,10 @@
 
 namespace App\Form;
 use App\Entity\Course;
+use App\Entity\Subject;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -31,7 +33,17 @@ class CourseType extends AbstractType
                 'required' => true, //nguoi dung phai nhap = nullable
                 'widget'=>'single_text'
             ]
-            );
+            )
+            
+            ->add('subject', EntityType::class,
+            [
+                'label' => "Subject",
+                'class' => Subject::class, 
+                'choice_label' => "name",   //show Author name in drop-down list
+                'multiple' => true,        //true: select many, false: select one
+                'expanded' => false         //true: checkbox   , false: drop-down list
+            ]);
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
