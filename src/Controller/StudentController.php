833516cb71c440efbc6f1d7b367900eac4d0d862
Qhuy9701 <87683532+Qhuy9701@users.noreply.github.com>
2021-10-22 16:51:19 +0700

    Controller
    Controller

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,19 +2,117 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Student;
+use App\Form\StudentType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StudentController extends AbstractController
 {
     /**
-     * @Route("/student", name="student")
-     */
-    public function index(): Response
+        * @Route("/student", name = "student_index")
+    */
+    public function studentIndexAction()
     {
-        return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
-        ]);
+        $students = $this->getDoctrine()->getRepository(Student::class)->findAll();
+        return $this->render(
+            'student/index.html.twig',
+            [
+                'students' => $students
+            ]
+        );
     }
-}
+
+    /**
+     * @Route("/student/Detail/{id}", name = "student_detail")
+     */
+    public function studentDetail($id) {
+        $student = $this->getDoctrine()->getRepository(Student::class)->find($id);
+        if ($student == null) {
+            $this->addFlash('Error', 'student is not existed');
+            return $this->redirectToRoute('student_index');
+        } else {  //$student != null
+            return $this->render(
+                'student/detail.html.twig', 
+                [
+                    'student' => $student
+                ]
+            );
+        }
+    }
+
+    /**
+     * @Route("/student/delete/{id}", name = "student_delete")
+     */
+    public function studentDelete ($id) {
+        $student = $this->getDoctrine()->getRepository(Student::class)->find($id);
+        if ($id == null) 
+        {
+            $this->addFlash('Error', 'student is not existed');
+        }
+        
+        else
+        {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($student);
+            $manager->flush();
+            $this->addFlash('Success', 'student has been deleted successfully !');   
+        }
+        return $this->redirectToRoute('student_index');
+    }
+
+    /**
+     * @Route("/student/add", name = "student_add")
+     */
+    public function studentAdd (Request $request) 
+    {
+        // $image = $book->getCover();
+        // $fileName= uniqid();
+        $student = new student();
+        $form = $this->createForm(studentType::class,$student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($student);
+            $manager->flush();
+
+            $this->addFlash("Success", "Add new sub student successfully !");
+            return $this->redirectToRoute('student_index');
+        }
+
+        return $this->render(
+            "student/add.html.twig", 
+            [
+                "form" => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/student/edit/{id}", name = "student_edit")
+     */
+    public function studentEdit (Request $request, $id) 
+    {
+        $student = $this->getDoctrine()->getRepository(student::class)->find($id);
+        $form = $this->createForm(studentType::class,$student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($student);
+            $manager->flush();
+
+            $this->addFlash("Success", "Edit student successfully !");
+            return $this->redirectToRoute('student_index');
+        }
+
+        return $this->render(
+            "student/edit.html.twig", 
+            [
+                "form" => $form->createView()
+            ]
+        );
+    }
+} 
--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -2,19 +2,115 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Subject;
+use App\Form\SubjectType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SubjectController extends AbstractController
 {
     /**
-     * @Route("/subject", name="subject")
-     */
-    public function index(): Response
+        * @Route("/subject", name = "subject_index")
+    */
+    public function subjectIndexAction()
     {
-        return $this->render('subject/index.html.twig', [
-            'controller_name' => 'SubjectController',
-        ]);
+        $subjects = $this->getDoctrine()->getRepository(Subject::class)->findAll();
+        return $this->render(
+            'subject/index.html.twig',
+            [
+                'subjects' => $subjects
+            ]
+        );
     }
-}
+
+    /**
+     * @Route("/subject/Detail/{id}", name = "subject_detail")
+     */
+    public function subjectDetail($id) {
+        $subject = $this->getDoctrine()->getRepository(Subject::class)->find($id);
+        if ($subject == null) {
+            $this->addFlash('Error', 'subject is not existed');
+            return $this->redirectToRoute('subject_index');
+        } else {  //$subject != null
+            return $this->render(
+                'subject/detail.html.twig', 
+                [
+                    'subject' => $subject
+                ]
+            );
+        }
+    }
+
+    /**
+     * @Route("/subject/delete/{id}", name = "subject_delete")
+     */
+    public function subjectDelete ($id) {
+        $subject = $this->getDoctrine()->getRepository(Subject::class)->find($id);
+        if ($id == null) 
+        {
+            $this->addFlash('Error', 'subject is not existed');
+        }
+        
+        else
+        {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($subject);
+            $manager->flush();
+            $this->addFlash('Success', 'subject has been deleted successfully !');   
+        }
+        return $this->redirectToRoute('subject_index');
+    }
+
+    /**
+     * @Route("/subject/add", name = "subject_add")
+     */
+    public function subjectAdd (Request $request) 
+    {
+        $subject = new Subject();
+        $form = $this->createForm(SubjectType::class,$subject);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($subject);
+            $manager->flush();
+
+            $this->addFlash("Success", "Add new sub subject successfully !");
+            return $this->redirectToRoute('subject_index');
+        }
+
+        return $this->render(
+            "subject/add.html.twig", 
+            [
+                "form" => $form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/subject/edit/{id}", name = "subject_edit")
+     */
+    public function subjectEdit (Request $request, $id) 
+    {
+        $subject = $this->getDoctrine()->getRepository(subject::class)->find($id);
+        $form = $this->createForm(subjectType::class,$subject);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($subject);
+            $manager->flush();
+
+            $this->addFlash("Success", "Edit subject successfully !");
+            return $this->redirectToRoute('subject_index');
+        }
+
+        return $this->render(
+            "subject/edit.html.twig", 
+            [
+                "form" => $form->createView()
+            ]
+        );
+    }
+} 
